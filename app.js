39119ee8f7d56c3d808c9aeff9e5b659284f5988
Umer Farooque <mohranvi4484@gmail.com>
2025-09-20 22:14:49 +0500

    optimized app functionality

--- app.js
+++ app.js
@@ -6,21 +6,51 @@ let time = () =>{
 let myInterval = setInterval(time, 1000);
 
 // let apiKey = "eb41e176c172278258865cfbba21fe57";
-let input = document.getElementById("input").value;
-// let inputBtn = document.getElementById("checkBtn");
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=eb41e176c172278258865cfbba21fe57&units=metric`;
 
 async function checkWeather(){
-    const response = await fetch(apiURL);
-    let data = await response.json();
-    console.log(data);
+    let weatherImage = document.getElementById("weatherImage");
+    let input = document.getElementById("input").value.trim();
+    if(!input) {
+        alert("Please enter a city name!");
+        return;
+    }
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=70f61d33bf6cd9eea2dc5f329e350704&units=metric`;
+    try{
+        const response = await fetch(apiURL);
+        if(!response.ok){
+            throw new Error("City not found!");
+        }
+        let data = await response.json();
+        console.log(data);
+        document.getElementById("temperature").innerHTML = Math.round(data.main.temp) + " °C";
+        document.getElementById("city").innerHTML = data.name;
+        document.getElementById("humidity").innerHTML = data.main.humidity + "%";
+        document.getElementById("wind").innerHTML = data.wind.speed + " km/h";
 
-    document.getElementById("temperature").innerHTML = Math.round(data.main.temp) + " °C";
-    document.getElementById("city").innerHTML = data.name;
-    document.getElementById("humidity").innerHTML = data.main.humidity + "%";
-    document.getElementById("wind").innerHTML = data.wind.speed + " km/h";
+        if(data.weather[0].main === "Clouds"){
+            weatherImage.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main === "Clear"){
+            weatherImage.src = "images/clear.png";
+        }
+        else if(data.weather[0].main === "Rain"){
+            weatherImage.src = "images/rain.png";
+        }
+        else if(data.weather[0].main === "Drizzle"){
+            weatherImage.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main === "Mist"){
+            weatherImage.src = "images/mist.png";
+        }
+        else if(data.weather[0].main === "Snow"){
+            weatherImage.src = "images/snow.png";
+        }
+    }
+    catch(error){
+        alert(error.message);
+    }
 
 }
-// inputBtn.addEventListener("click", () => {
-//     checkWeather();
-// });
+
+let inputBtn = document.getElementById("checkBtn");
+inputBtn.addEventListener("click",  checkWeather);
